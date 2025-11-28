3f96d0da94ac698853fac683009320cc5084bfe4
OnkarAK176 <Karandeonkar0@gmail.com>
2025-11-28 10:29:47 +0530

    Update settings.py

--- tweet/tweet/settings.py
+++ tweet/tweet/settings.py
@@ -126,7 +126,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 
 LOGIN_REDIRECT_URL = '/'
